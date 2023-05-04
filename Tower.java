243d52e82a65f900d6a6cdcc4b944d907c808067
Alexia <donati.alexia@gmail.com>
2023-05-04 19:08:28 +0200

    Added Tower and TowerManager class

---
+++ Tower.java
@@ -0,0 +1,85 @@
+import javax.swing.*;
+
+abstract class Tower extends ArmedEntity{
+    protected int cost;
+    protected int unlock;
+    protected int decay;
+
+    protected ImageIcon sprite;
+
+    @Override
+    public Tower clone() throws CloneNotSupportedException {
+        return (Tower) super.clone();
+    }
+
+    public int get_cost(){ return cost; }
+
+    public ImageIcon get_sprite(){ return sprite; }
+    abstract void power();
+}
+
+class Tower1 extends Tower {
+    Tower1() {
+        sprite = new ImageIcon("../resources/towers/tower1.png");
+        cost = 10;
+        // TODO set variables to the right start values
+    }
+
+    @Override
+    public Tower clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public void power(){
+        // TODO implement power
+    }
+}
+
+class Tower2 extends Tower {
+    Tower2() {
+        sprite = new ImageIcon("../resources/towers/tower2.png");
+        cost = 110;
+        // TODO set variables to the right start values
+    }
+
+    @Override
+    public Tower clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public void power(){
+        // TODO implement power
+    }
+}
+
+class Tower3 extends Tower {
+    Tower3() {
+        sprite = new ImageIcon("../resources/towers/tower3.png");
+        // TODO set variables to the right start values
+    }
+
+    @Override
+    public Tower clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public void power(){
+        // TODO implement power
+    }
+}
+
+class Tower4 extends Tower {
+    Tower4() {
+        sprite = new ImageIcon("../resources/towers/tower4.png");
+        // TODO set variables to the right start values
+    }
+
+    @Override
+    public Tower clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public void power(){
+        // TODO implement power
+    }
+}
---
+++ TowerManager.java
@@ -0,0 +1,65 @@
+import graphics.TowerDefenseView;
+import graphics.exceptions.EmptySpriteException;
+import graphics.exceptions.WrongTowerPositionException;
+
+import javax.swing.*;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TowerManager {
+    private TowerDefenseView view;
+
+    static final int nbrType = 4;
+    private Tower[] towerType;
+
+    private int nbrTower;
+    private List<Tower> listTower;
+
+    TowerManager(TowerDefenseView view) {
+        this.view = view;
+
+        towerType = new Tower[nbrType];
+
+        towerType[0] = new Tower1();
+        towerType[1] = new Tower2();
+        towerType[2] = new Tower3();
+        towerType[3] = new Tower4();
+
+        nbrTower = 0;
+        listTower = new ArrayList<Tower>();
+    }
+
+    public int get_cost(int towerIndex){ return towerType[towerIndex].get_cost(); }
+    public ImageIcon get_sprite(int towerIndex){ return towerType[towerIndex].get_sprite(); }
+
+    void add_Tower(int x, int y, int towerIndex){
+        try {
+            Tower newTower = towerType[towerIndex].clone();
+            newTower.set_position(x, y);
+
+            listTower.add(newTower);
+            nbrTower++;
+        } catch (CloneNotSupportedException e){
+            e.printStackTrace();
+        }
+
+        try {
+            view.updateTowerField(x, y, get_sprite(towerIndex), 0);
+        } catch (WrongTowerPositionException | EmptySpriteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update(){
+        for(Tower t : listTower){
+            Point2D pos = t.get_position();
+            System.out.print((int) pos.getX());
+            try {
+                view.updateTowerField((int) pos.getX(), (int) pos.getY(), t.get_sprite(), 0);
+            } catch (WrongTowerPositionException | EmptySpriteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
