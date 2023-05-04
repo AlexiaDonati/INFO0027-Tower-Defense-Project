dbe3962b6e82ff12f5130bf1091695bdf98d0807
Alexia <donati.alexia@gmail.com>
2023-05-04 20:30:28 +0200

    make GameState an interface

--- GameState.java
+++ GameState.java
@@ -1,26 +1,28 @@
-abstract public class GameState {
+abstract public interface GameState {
     abstract void launchWave(Game game);
 
     abstract void play(Game game);
 }
 
-class PlayingState extends GameState {
+class PlayingState implements GameState {
+
     public void launchWave(Game game) {
         System.out.print("You can't launch a new wave while one is still ongoing\n");
     }
-    void play(Game game){
+
+    public void play(Game game){
         game.update();
     }
 }
 
-class PlacingState extends GameState {
+class PlacingState implements GameState {
 
     public void launchWave(Game game){
         game.initWave();
         game.set_state(new PlayingState());
     }
 
-    void play(Game game){
+    public void play(Game game){
         return;
     }
 }
