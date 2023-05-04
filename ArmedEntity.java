e6a4de9e8c09cb272ca5446ff378145541381d66
Alexia <donati.alexia@gmail.com>
2023-05-04 19:04:30 +0200

    Added a superclass ArmedEntity for both Enemy and Tower

---
+++ ArmedEntity.java
@@ -0,0 +1,42 @@
+import java.awt.geom.Point2D;
+public class ArmedEntity implements Cloneable{
+    private Point2D position;
+
+    protected int range;
+    protected int damage;
+    protected int rate;
+
+    ArmedEntity(){
+        position = new Point2D.Double(0, 0);
+    }
+
+    @Override
+    public ArmedEntity clone() throws CloneNotSupportedException {
+        ArmedEntity clone = (ArmedEntity) super.clone();
+        clone.position = (Point2D) position.clone();
+        return clone;
+    }
+
+    public void set_position(int x, int y){
+        position.setLocation(x, y);
+    }
+
+    public Point2D get_position(){
+        return position;
+    }
+
+    private boolean can_reach(int x, int y){
+        return range >= position.distance(x, y);
+    }
+
+    private boolean can_fire(int currTime){
+        return !(currTime % rate == 0);
+    }
+
+    public int try_to_hit(int x, int y, int currTime){
+        if(can_reach(x, y) && can_fire(currTime)){
+            return damage;
+        }
+        return 0;
+    }
+}
