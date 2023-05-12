504fc88bac66a342c0e67a590d8b2e66e2908188
Alexia <donati.alexia@gmail.com>
2023-05-12 11:15:42 +0200

    Making Cell more general so it can also work for the bonus of the enemy attacking towers

--- Cell.java
+++ Cell.java
@@ -1,34 +1,40 @@
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
 
-public class Cell {
-    private Set<Tower> towers;
-    private List<Enemy> enemies;
+abstract class CellSubject{
+    protected List<Observer> observers = new ArrayList<>();
+
+    public void attach_Observer(Observer o){
+        observers.add(o);
+    }
+
+    public void detach_Observer(Observer o){
+        observers.remove(o);
+    }
+
+    public void notify_observers(){
+        for(Observer o : observers){
+            o.update();
+        }
+    }
+}
+
+public class Cell{
+    private List<ArmedEntity> entities;
 
     public Cell(){
-        towers = new HashSet<>();
-        enemies = new ArrayList<>();
+        entities = new ArrayList<>();
     }
 
-    public void attach_Tower(Tower tower){
-        towers.add(tower);
+    public void add_Entity(ArmedEntity e){
+        entities.add(e);
     }
 
-    public void detach_Tower(Tower tower){
-        towers.remove(tower);
+    public void remove_Entity(ArmedEntity e){
+        entities.remove(e);
     }
 
-    public void add_Enemy(Enemy enemy){
-        enemies.add(enemy);
-
+    public List<ArmedEntity> give_update(){
+        return entities;
     }
-
-    public void remove_Enemy(Enemy enemy){
-        enemies.remove(enemy);
-    }
-    /*
-    public Enemy give_Update(){
-        Enemy enemy;
-        //do smthg
-        return enemy;
-    }*/
 }
